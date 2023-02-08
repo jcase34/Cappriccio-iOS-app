1c91ae9d057ad68484e435e17165fc5d89cc133c
jacob <jcase.bus@gmail.com>
2023-02-08 10:49:00 -0800

    Added navigation controllers before new tab bar view controller options.
    Updated title to "Cappriccio" on navigation bar titles.

---
+++ Lento/Controller/MainTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabBarController.swift
+//  Lento
+//
+//  Created by Jacob Case on 2/8/23.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.navigationItem.title = "My Title"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
