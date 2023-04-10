70f12b9b82e69b0ac0333fab65178b45a34154c9
jacob <jcase.bus@gmail.com>
2023-04-10 11:43:10 -0700

    Added CoreDataManager file that was missing from project. Need to re-incorporate all missing functions in file.

---
+++ Lento/Utilities/CoreDataManager.swift
@@ -0,0 +1,66 @@
+//
+//  CoreDataManager.swift
+//  Lento
+//
+//  Created by Jacob Case on 4/10/23.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    private init() { }
+    
+    
+    func insertMainPiece(mainPiece: String) {
+        
+    }
+    
+    func insertSightReading(sightReading: String) {
+        
+    }
+    
+    func insertImprovsation(improvsation: String) {
+        
+    }
+    
+    func insertRepertoire(repertoire: String) {
+        
+    }
+    
+    func updatePracticeSession() {
+        
+    }
+    
+    func insertPracticeSession() {
+        
+    }
+    
+    func fetchSessionItems() {
+        
+    }
+    
+    func deleteSessionItem() {
+        
+    }
+    
+    func deletePracticeSession(practiceSession: PracticeSession) {
+        
+    }
+    
+    func fetchSortedPracticeSessionsByDate() {
+        
+    }
+    
+    func fetchTotalPracticeSessiondMinutes() {
+        
+    }
+    
+    func fetchTotalPracticeSessionCount() {
+        
+    }
+    
+}
